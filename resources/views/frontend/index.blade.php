534bd50839c3f7866a93173487da8a22deff5027
Shadi Abdelslam <shadodev@gmail.com>
2021-08-14 00:36:44 +0200

    Edit chat, checkout, shop pages in frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,7 +9,7 @@
             <div class="col-lg-6">
                 <p class="text-muted small text-uppercase mb-2">New Inspiration 2020</p>
                 <h1 class="h2 text-uppercase mb-3">20% off on new season</h1><a class="btn btn-dark"
-                    href="shop.html">Browse collections</a>
+                    href="{{ route('frontend.shop') }}">Browse collections</a>
             </div>
         </div>
     </div>
@@ -21,13 +21,13 @@
         <h2 class="h5 text-uppercase mb-4">Browse our categories</h2>
     </header>
     <div class="row">
-        <div class="col-md-4 mb-4 mb-md-0"><a class="category-item" href="shop.html"><img class="img-fluid"
+        <div class="col-md-4 mb-4 mb-md-0"><a class="category-item" href="{{ route('frontend.shop') }}"><img class="img-fluid"
                     src="{{ asset('frontend/img/cat-img-1.jpg') }}" alt=""><strong class="category-item-title">Clothes</strong></a></div>
-        <div class="col-md-4 mb-4 mb-md-0"><a class="category-item mb-4" href="shop.html"><img class="img-fluid"
+        <div class="col-md-4 mb-4 mb-md-0"><a class="category-item mb-4" href="{{ route('frontend.shop') }}"><img class="img-fluid"
                     src="{{asset('frontend/img/cat-img-2.jpg') }}" alt=""><strong class="category-item-title">Shoes</strong></a><a
-                class="category-item" href="shop.html"><img class="img-fluid" src="{{asset('frontend/img/cat-img-3.jpg') }}" alt=""><strong
+                class="category-item" href="{{ route('frontend.shop') }}"><img class="img-fluid" src="{{asset('frontend/img/cat-img-3.jpg') }}" alt=""><strong
                     class="category-item-title">Watches</strong></a></div>
-        <div class="col-md-4"><a class="category-item" href="shop.html"><img class="img-fluid" src="{{asset('frontend/img/cat-img-4.jpg') }}"
+        <div class="col-md-4"><a class="category-item" href="{{ route('frontend.shop') }}"><img class="img-fluid" src="{{asset('frontend/img/cat-img-4.jpg') }}"
                     alt=""><strong class="category-item-title">Electronics</strong></a></div>
     </div>
 </section>
