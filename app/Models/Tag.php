3b6036e32ebf3a55e719760887a1f2c83d233896
Shadi Abdelslam <shadodev@gmail.com>
2021-09-02 18:58:09 +0200

    Tags Section
    - Complete TagController
    - Complete Tag Model
    - Complete TagRequest
    - Complete tags Views
    - Create filter.blade in tags backend views

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Tag extends Model
 {
-    use HasFactory, Sluggable;
-
+    use HasFactory, Sluggable, SearchableTrait;
     protected $guarded = [];
 
     public function sluggable(): array
@@ -19,6 +20,17 @@ class Tag extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    protected $searchable = [
+        'columns' => [
+            'tags.name' => 10,
+        ]
+    ];
+
+    public function status()
+    {
+        return $this->status ? 'Active' : 'Inactive';
     }
 
     public function products(): MorphToMany
