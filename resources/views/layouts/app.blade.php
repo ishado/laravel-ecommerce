9cef14dca0c75eea3d569b2b4fad9c65a435d2ee
Shadi Abdelslam <shadodev@gmail.com>
2021-09-07 20:56:00 +0200

    Installation Livewire package

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,8 @@
     <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-
+    @livewireStyles
+    @yield('style')
 </head>
 
 <body>
@@ -86,7 +87,8 @@
             injectSvgSprite('https://bootstraptemple.com/files/icons/orion-svg-sprite.svg');
 
     </script>
-
+    @livewireScripts
+    @yield('script')
 </body>
 
 </html>
